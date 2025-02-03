2d01a305e35290369b570a4f3c52fbc8f5f5c7c5
Opilane TTHK <opilane@tthk.ee>
2025-02-03 15:37:07 +0200

    ifelsecolor
    IfElseColor ülesanne 03.02.2025

---
+++ ifelsecolor/ifelsecolor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose your color - ");
+            string color = Console.ReadLine();
+            
+
+            if (color == "red" || color == "green" || color == "white" || color == "blue")
+            {
+              Console.WriteLine("Color that you chose is - " + color);
+            }
+            else
+            {
+            Console.WriteLine("You cant choose this color!");
+                Console.Beep();
+            }
+        }
+    }
+}
+
+
