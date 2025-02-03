d3d72611ea7d7dca968eb125523b18f86f76b985
Opilane TTHK <opilane@tthk.ee>
2025-02-03 14:01:51 +0200

    ifElseNesting
    123123

---
+++ ifelsenesting/ifelsenesting/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ifelsenesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
