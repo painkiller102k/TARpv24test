f3528008bae6683722aaed10fc648ed1e18f4cd0
Opilane TTHK <opilane@tthk.ee>
2025-02-03 14:49:41 +0200

    ifelsenesting1
    wqeqweqwe

--- ifelsenesting/ifelsenesting/Program.cs
+++ ifelsenesting/ifelsenesting/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Number Control!");
+
+            int number = 189;
+
+            if (number > 189)
+            {
+                Console.WriteLine("Number is bigger");
+
+                if (number > 190 && number < 200)
+                {
+                    Console.WriteLine("Number is between 190 and 200");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Number is smaller");
+            }
         }
-    }
+     }
 }
+
+
+
